e2c1be83a487c659829cafe58ffe59500ddd94cc
Joseph Atkins-Turkish <spacerat3004@gmail.com>
2022-03-11 12:51:06 -0800

    Display a progress bar

--- pages/sort.tsx
+++ pages/sort.tsx
@@ -39,8 +39,13 @@ const Again = styled.div`
   text-align: center;
 `;
 
+function ProgressBar({value}: {value: number}) {
+  const percentage = value * 100
+  return <progress value={percentage} max="100" style={{width: "100%"}} aria-label={"Sort Progress"}>{percentage}%</progress>
+}
+
 function SortLayout({ state }: { state: SortAppState }) {
-  const { pick, status, addItem, removeItem, clearCache, canUndo, undo } =
+  const { pick, status, addItem, removeItem, clearCache, canUndo, undo, progress } =
     state;
 
   useKeyboardSupport(status, undo, pick);
@@ -53,6 +58,7 @@ function SortLayout({ state }: { state: SortAppState }) {
 
   return (
     <Page>
+      <ProgressBar value={progress} />
       <H1>What&apos;s Better?</H1>
       <SideBySideButtons>
         <Button onClick={() => pick(a)}>{a}</Button>
