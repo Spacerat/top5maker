e203847e9ecc917e1cf4aa5a300fb78dc748e2f9
Joseph Atkins-Turkish <spacerat3004@gmail.com>
2022-03-11 13:03:32 -0800

    rm dead import

--- lib/interruptibleSort/index.ts
+++ lib/interruptibleSort/index.ts
@@ -1,2 +1,2 @@
-export { cacheWithUpdate, heapSort, initCache } from "./interruptibleHeapsort";
+export { cacheWithUpdate, heapSort } from "./interruptibleHeapsort";
 export type { SortCache, SortStatus } from "./interruptibleHeapsort";
