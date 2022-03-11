7fad8be6d1c1c7fb9a0defe2a4ceb8c61ce7f31e
Joseph Atkins-Turkish <spacerat3004@gmail.com>
2022-03-11 15:48:20 -0800

    Scale the metric

--- app/useSortState.tsx
+++ app/useSortState.tsx
@@ -24,8 +24,16 @@ import {
 import { useSortUrl } from "./useSortUrl";
 
 function getSortedness(cache: Graph, maxItems: number) {
-  return Math.sqrt(
-    sumFamilialConnections(cache) / maxFamilialConnections(maxItems)
+  // TODO: think a bit more than this
+  // The number of familial connections os O(N*2)
+  // Whereas the the number of comparisons needed is O(N log N)
+  // Therefore, this should rescale the metric to be linear in number of comaprisons...
+  // ... I think?
+
+  const scale = (n: number) => Math.sqrt(n) * Math.log2(n);
+  return (
+    scale(sumFamilialConnections(cache)) /
+    scale(maxFamilialConnections(maxItems))
   );
 }
 
