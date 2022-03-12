b7e638e7cdfaafed5d2411b1e4a819fc42ce06c2
Joseph Atkins-Turkish <spacerat3004@gmail.com>
2022-03-12 15:02:06 -0800

    Fix #2: unicode errors

--- lib/base64.ts
+++ lib/base64.ts
@@ -21,14 +21,42 @@ const base64decode: (str: string) => string =
 
 /** Encode a string as URL-safe base64 */
 export function safe64encode(data: string) {
-  return encode64toSafe(base64encode(data));
+  return encode64toSafe(base64encode(toBinary(data)));
 }
 /** Decode a string from URL-safe base64 */
 export function safe64decode(data: string) {
-  return base64decode(decode64fromSafe(data));
+  return fromBinary(base64decode(decode64fromSafe(data)));
 }
 
 /** Return true if the object is a URL-safe base64 string */
 export function isSafeBase64(data: unknown): data is string {
   return typeof data === "string" && !!isUrlSafeBase64(data);
+}
+
+// From: https://developer.mozilla.org/en-US/docs/Web/API/btoa
+function toBinary(string: string) {
+  const codeUnits = new Uint16Array(string.length);
+  for (let i = 0; i < codeUnits.length; i++) {
+    codeUnits[i] = string.charCodeAt(i);
+  }
+  const charCodes = new Uint8Array(codeUnits.buffer);
+  let result = '';
+  for (let i = 0; i < charCodes.byteLength; i++) {
+    result += String.fromCharCode(charCodes[i]);
+  }
+  return result;
+}
+
+// From: https://developer.mozilla.org/en-US/docs/Web/API/btoa
+function fromBinary(binary: string) {
+  const bytes = new Uint8Array(binary.length);
+  for (let i = 0; i < bytes.length; i++) {
+    bytes[i] = binary.charCodeAt(i);
+  }
+  const charCodes = new Uint16Array(bytes.buffer);
+  let result = '';
+  for (let i = 0; i < charCodes.length; i++) {
+    result += String.fromCharCode(charCodes[i]);
+  }
+  return result;
 }
