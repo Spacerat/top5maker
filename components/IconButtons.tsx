5a2859fe1b1907299d3b6c08c93737d1013f5ad6
Joseph Atkins-Turkish <spacerat3004@gmail.com>
2022-03-14 11:22:01 -0700

    Refactor IconButtons to use Icons

--- components/IconButtons.tsx
+++ components/IconButtons.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { CrossIcon, RedoIcon } from "./Icons";
 
 export const IconButton = styled.button`
   font-size: ${({ theme }) => theme.typography.body.fontSize};
@@ -48,17 +49,7 @@ export function RemoveItemButton({
       aria-label={`Remove item: ${item}`}
       title="Remove item"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        width="16"
-        height="16"
-      >
-        <path
-          fill="currentColor"
-          d="M23.707.293a1 1 0 0 0-1.414 0L12 10.586 1.707.293a1 1 0 0 0-1.414 0 1 1 0 0 0 0 1.414L10.586 12 .293 22.293a1 1 0 0 0 0 1.414 1 1 0 0 0 1.414 0L12 13.414l10.293 10.293a1 1 0 0 0 1.414 0 1 1 0 0 0 0-1.414L13.414 12 23.707 1.707a1 1 0 0 0 0-1.414Z"
-        />
-      </svg>
+      <CrossIcon />
     </IconButton>
   );
 }
@@ -77,18 +68,7 @@ export function RedoItemButton({
       aria-label={`Re-sort item: ${item}`}
       title="Re-sort item"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        xmlSpace="preserve"
-        width="16"
-        height="16"
-        viewBox="0 0 24 24"
-      >
-        <path
-          fill="currentColor"
-          d="M21.962 12.875A10.03 10.03 0 1 1 19.122 5H16a1 1 0 0 0-1 1 1 1 0 0 0 1 1h4.143A1.858 1.858 0 0 0 22 5.143V1a1 1 0 0 0-1-1 1 1 0 0 0-1 1v2.078A11.985 11.985 0 1 0 23.95 13.1a1.007 1.007 0 0 0-1-1.1.982.982 0 0 0-.988.875Z"
-        />
-      </svg>
+      <RedoIcon />
     </IconButton>
   );
 }
