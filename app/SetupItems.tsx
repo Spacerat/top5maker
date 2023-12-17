c8966cf51be41df4385c448aa659846200cd57c0
Joseph Atkins-Turkish <spacerat3004@gmail.com>
2023-12-16 23:29:10 -0800

    Extract SetupItems from start page

---
+++ app/SetupItems.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import { MAX_ITEMS } from "@/sortState/config";
+import { useSortUrl } from "@/sortState/useSortUrl";
+import { AddForm } from "@/components/AddItemInput";
+import { FullMobileButton } from "@/components/Button";
+import { ItemList } from "@/components/List";
+import { stringSetRemove, stringSetUnion } from "@/lib/immutableStringSet";
+
+export function SetupItems() {
+  const [items, setItems] = React.useState<readonly string[]>([]);
+
+  const addItems = (names: readonly string[]) => {
+    setItems((curItems) => stringSetUnion(curItems, names, MAX_ITEMS));
+  };
+
+  const removeItem = (name: string) => {
+    setItems((curItems) => stringSetRemove(curItems, name));
+  };
+
+  const sortUrl = useSortUrl(items);
+
+  return (
+    <>
+      <ItemList items={items} onRemove={removeItem} />
+      <AddForm
+        onAddItems={addItems}
+        keepInView
+        disabled={items.length >= MAX_ITEMS}
+      />
+      <Link passHref href={sortUrl} legacyBehavior>
+        <FullMobileButton disabled={items.length < 3}>
+          Start Sorting
+        </FullMobileButton>
+      </Link>
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,12 @@
-"use client";
-
-import Head from "next/head";
 import Link from "next/link";
 import React from "react";
-import { MAX_ITEMS } from "@/sortState/config";
-import { makeSortUrl, useSortUrl } from "@/sortState/useSortUrl";
-import { AddForm } from "@/components/AddItemInput";
+import { makeSortUrl } from "@/sortState/useSortUrl";
 import { Brand, Footer, Illustration, TagLine } from "@/components/Brand";
-import { FullMobileButton } from "@/components/Button";
 import { Card, CardGrid, Header, Main, Page } from "@/components/layout";
-import { ItemList } from "@/components/List";
 import { H3 } from "@/components/text";
-import { stringSetRemove, stringSetUnion } from "@/lib/immutableStringSet";
+import { SetupItems } from "./SetupItems";
 
-function SetupItems() {
-  const [items, setItems] = React.useState<readonly string[]>([]);
-
-  const addItems = (names: readonly string[]) => {
-    setItems((curItems) => stringSetUnion(curItems, names, MAX_ITEMS));
-  };
-
-  const removeItem = (name: string) => {
-    setItems((curItems) => stringSetRemove(curItems, name));
-  };
-
-  const sortUrl = useSortUrl(items);
-
-  return (
-    <>
-      <ItemList items={items} onRemove={removeItem} />
-      <AddForm
-        onAddItems={addItems}
-        keepInView
-        disabled={items.length >= MAX_ITEMS}
-      />
-      <Link passHref href={sortUrl} legacyBehavior>
-        <FullMobileButton disabled={items.length < 3}>
-          Start Sorting
-        </FullMobileButton>
-      </Link>
-    </>
-  );
-}
+import styles from "./page.module.css";
 
 function example(name: string, items: readonly string[]) {
   return { items, url: makeSortUrl(items), display: items.slice(0, 3), name };
@@ -107,36 +72,30 @@ function ExampleCard(example: Example) {
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Sort Star</title>
-      </Head>
-      <Main>
-        <Header>
-          <div>
-            <Brand />
-            <TagLine />
-          </div>
-          <Illustration />
-        </Header>
+    <Main>
+      <Header>
+        <div className={styles.taglineContainer}>
+          <Brand />
+          <TagLine />
+        </div>
+        <Illustration />
+      </Header>
 
-        <Page>
-          <H3>Add three or more items to get started</H3>
-          You can add items one by one, or paste multiple lines
-          <SetupItems />
-        </Page>
-        <Page kind="darker">
-          <H3>Or try one of these examples</H3>
-          <CardGrid>
-            <ExampleCard {...foods} />
-            <ExampleCard {...names} />
-            <ExampleCard {...business} />
-            <ExampleCard {...powers} />
-          </CardGrid>
-        </Page>
-
-        <Footer />
-      </Main>
-    </>
+      <Page>
+        <H3>Add three or more items to get started</H3>
+        You can add items one by one, or paste multiple lines
+        <SetupItems />
+      </Page>
+      <Page kind="darker">
+        <H3>Or try one of these examples</H3>
+        <CardGrid>
+          <ExampleCard {...foods} />
+          <ExampleCard {...names} />
+          <ExampleCard {...business} />
+          <ExampleCard {...powers} />
+        </CardGrid>
+      </Page>
+      <Footer />
+    </Main>
   );
 }
