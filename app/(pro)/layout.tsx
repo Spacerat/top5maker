98eebe657ef50dd31ebeb76f11afc143904d19e5
Joseph Atkins-Turkish <spacerat3004@gmail.com>
2023-12-23 22:33:53 -0800

    Use pro brand

--- app/(pro)/layout.tsx
+++ app/(pro)/layout.tsx
@@ -1,4 +1,4 @@
-import { Brand } from "@/components/Brand";
+import { ProBrand } from "@/components/Brand";
 import { Header, Main } from "@/components/layout";
 import { NavLink } from "@/components/text";
 
@@ -7,7 +7,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <Main>
       <Header>
         <div className="flex flex-row items-baseline gap-12">
-          <Brand />
+          <ProBrand />
           <div className="flex flex-row items-baseline gap-8">
             <NavLink href={"/lists"}>Your Lists</NavLink>
           </div>
