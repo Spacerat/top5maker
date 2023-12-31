4f7aecc8307b4a990dbd2b12135a94d3bfc3767a
Joseph Atkins-Turkish <spacerat3004@gmail.com>
2023-12-31 15:31:46 -0800

    Fix button style

--- components/SideBySideButtons.tsx
+++ components/SideBySideButtons.tsx
@@ -1,4 +1,4 @@
 import styles from "./SideBySideButtons.module.css";
 import { styled } from "./withStyle";
 
-export const SideBySideButtons = styled.div(styles.sideBySideButton);
+export const SideBySideButtons = styled.div(styles.sideBySideButtons);
